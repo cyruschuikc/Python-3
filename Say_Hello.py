0d3ba3853b3d8feea238fd4f69d8a6bcd02660f9
Chui-Cyrus C.K.C <cyruschuikc@gmail.com>
2024-08-19 13:59:33 +0800

    Say_Hello.py
    In this document, we will learn how to print a message in the console.

---
+++ Say_Hello.py
@@ -0,0 +1,20 @@
+### Lesson-01: Say 'Hello'
+## In this lesson, we will learn how to print a message in the console.
+## Frist, we might use the print() function.
+print("Hello!") # print a word
+## Additionally, we can print a short sentence or a complete sentence.
+print("Hello, my name is Chris.")
+print("Hello, my name is Chris, 16s. I'm a student.")
+## Here are an commonly example.
+print("Hello, World.")
+## However, please pay attention here. Because '' and "" although is the same.
+## But there are some differences in their usage. Here are some examples.
+print('Hello!') # Use ''
+print("Hello!") # Use ""
+print("'Hello'") # Use '' in ""
+print('"Hello"') # Use "" in ''
+## All in all, we can use either '' or "" to print a message. 
+## But, sometimes we will use '' to surround certain words in a text.
+## So, We should get used to using "" first when outputting words or a sentence.
+## This is the end of this lesson. Thank you for your attention. 
+## If you feel interested, you can follow me. I will update content as soon as possible.
