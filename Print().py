633f6a3dc920d3644b461ae433469fab741ed102
Chui-Cyrus C.K.C <cyruschuikc@gmail.com>
2024-08-19 15:35:58 +0800

    Print().py
    In this document, we will learn more about the print() function.

---
+++ Print().py
@@ -0,0 +1,21 @@
+### Lesson-02: Print()
+## At the last lesson we also had used print() function to print some of things.
+## However, in this lesson, we will learn more about the print() function.
+## The print() function is used to print anything in the console.
+## While we want to print a text, we need to use print() and put the text inside the ().
+## For example:
+print("ABC")
+## Also, you can use '' to do it.
+print('ABC')
+## Furthermore, you can print a number as integer or float.
+print(123)
+print(1.23)
+## We can also print a variable,boolean,one-dimensional array and two-dimensional array.
+letter = "ABC"
+print(letter)
+print(True)
+print(False)
+print([1,2,3,4,5])
+print([[1,2,3],[4,5,6]])
+## This is the end of this lesson. Thank you for your attention. 
+## If you feel interested, you can follow me. I will update content as soon as possible.
