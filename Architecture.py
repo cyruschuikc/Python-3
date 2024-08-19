2bb59694d508a91ed4f4ba1e7bd2a2279b7342f5
Chui-Cyrus C.K.C <cyruschuikc@gmail.com>
2024-08-19 13:05:15 +0800

    Architecture.py
    In this document, we will learn about the architecture of a same Python-3 program.

---
+++ Architecture.py
@@ -0,0 +1,41 @@
+### Lesson-00: "Architecture"
+
+## In this program, we will learn about the architecture of a same Python-3 program.
+
+class class_name: # Create a Class
+  def fx(self): 
+    # Defined function
+    # You can set some of the conditions, events, or both in here.
+    pass
+  # Also, you can set more functions in here unless your program may need.
+  pass
+
+obj = class_name() # Create Object.
+
+# Call the fx method on the instance
+obj.fx()
+
+str = "Hello World!" # String
+init = 0,1,2,3,4,5,6,7,8,9 # Integer
+num = 0.0,1.0,2.0,3.0,4.0,5.0 # Float
+bool = True,False # Boolean
+list = [1,2,3,4,5,6,7,8,9,0] # List
+array = [[1,2,3],[4,5,6],[7,8,9],['',0,'']] # 2D array
+
+## Now, let's we try to show out the str, init, num, bool, list, and array.
+print(str)
+for i in range(10): # For loop
+  print(init[i])
+for i in range(6):
+  print(num[i])
+print(bool)
+print(list)
+for i in range(4):
+  print(array[i])
+
+while True: # While loop
+  break
+
+## The above is a basic example structure.
+## This is the end of this lesson. Thank you for your attention. 
+## If you feel interested, you can follow me. I will update content as soon as possible.
