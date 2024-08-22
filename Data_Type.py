f9f3cc7b8ce9393d626a5cd2a59539d764207767
Chui-Cyrus C.K.C <cyruschuikc@gmail.com>
2024-08-22 14:23:20 +0800

    Data_Type.py
    In this document, we will learn about the data type in python.

---
+++ Data_Type.py
@@ -0,0 +1,35 @@
+### Lesson-03: Data Type
+## In this lesson, we will learn about the data type in python.
+## Frist, we might make sure to know about all the data type, such as:
+## 1. Integer
+## 2. Float
+## 3. String
+## 4. Boolean
+
+# Integer number:
+## Integer number is a number without decimal point.
+INT_num = 1
+
+# Float:
+## Float is a number with decimal point.
+FLOAT_num = 2.34
+
+# String:
+## String is a text.
+text = "Hello, World!"
+
+# Boolean:
+## Boolean is a True or False.
+booleanT = True
+booleanF = False
+
+## When our program have a TypeError, we might need to check the data type of variable or function.
+## In that case, We can use the type() function to check the data type. Type is a function that will show the data type.
+print(type(INT_num))
+print(type(FLOAT_num))
+print(type(text))
+print(type(booleanT))
+print(type(booleanF))
+
+## This is the end of this lesson. Thank you for your attention. 
+## If you feel interested, you can follow me. I will update content as soon as possible.
