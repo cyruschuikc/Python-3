bf0de4c45e6ae36f3226b52818cd7150b1000b65
Chui-Cyrus C.K.C <cyruschuikc@gmail.com>
2024-08-27 16:59:54 +0800

    Arithmetic_Operators.py
    In this document, we will learn about the operators in python.

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,14 @@
+### Lesson-05: Operator
+## In this lesson, we will learn about the operators in python.
+## Arithmetic Operators:
+# +, -, *, /, //, %, **
+print("1+2=",1+2)
+print("3-4=",3-4)
+print("5*6=",5*6)
+print("7/8=",7/8)
+print("9//10=",9//10)
+print("1%2=",11%12)
+print("2**3=",2**3)
+## The operators can help us to do easily calculations.
+## This is the end of this lesson. Thank you for your attention. 
+## If you feel interested, you can follow me. I will update content as soon as possible.
